9b46dc6b2db20a8d67d18c7c0333ea09bd3a9777
Mayur Mudigonda <mayur@computer.org>
2014-05-12 14:56:13 -0700

    Latest work

--- sesemo.py
+++ sesemo.py
@@ -14,42 +14,56 @@ Mayur Mudigonda, April 16th 2014
 
 """
 
-import os
-import sys
-import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import minimize
+import time
 
 
 class SesemoAtom:
     
-    def __init__(self,pathtype=None, samples=None):
+    def __init__(self,pathtype=None, samples=None, iterations=None, learnMotor=None, learnSensory=None):
         
         if pathtype is None:
             self.pathtype='Default'
             
         if samples is None:
             self.samples = 10000
+        else:
+            self.samples = samples
                             
+        if iterations is None:
+            self.learnIterations = 100
+        else:
+            self.learnIterations = iterations                        
         self.FOV = 2 # defines a FOV x FOV mask as the window
         self.x,self.y = self.getData()
         self.center = np.zeros([1,2]) #defines where the center of the camer is at this point
         self.center[0][0] = self.x[0]
         self.center[0][1] = self.y[0]
-        self.M = self.motorBasis()
-        self.S = self.sensoryBasis()
+        if learnMotor is None:
+            self.M = self.motorBasis()
+        else:
+            self.M = np.random.randn(learnMotor,2)
+        if learnSensory is None:
+            self.S = self.sensoryBasis()
+        else:
+            self.S = np.random.randn(learnSensory,2)
+                
         #Inferred coffecients for Sensory percept
         self.alpha = np.zeros([self.samples,np.shape(self.S)[0]],dtype=float) 
         #Inferred cofficents for Motor representations
-        self.beta = np.zeros([self.samples,np.shape(self.M)[0]],dtype=float)
+        self.beta = np.zeros([self.samples+1,np.shape(self.M)[0]],dtype=float)
         self.G = np.random.randn(np.shape(self.S)[0],np.shape(self.M)[0]) #Going between sensory and motor repr. spaces 
         self.F = np.random.randn(np.shape(self.M)[1],np.shape(self.S)[1]) #Going from Motor Space to Camera Space
         self.TimeIdx = 0
         self.lam1 = .1
-        self.lam2 = .91
-        self.learnIterations = 1000
+        self.LR = .1
+        self.Error = np.zeros([self.learnIterations],dtype=float)
         self.DEBUG = True
+        #self.fig = plt.figure()
+        #ax = plt.axes(xlim=(-10,10),ylim=(-10,10))
+        #self.anim, = ax.plot([],[],'g') 
             
     def getData(self):
         
@@ -63,7 +77,11 @@ class SesemoAtom:
             y = r*np.sin(angle_range)
             
             return x,y
-            
+    '''           
+    def visualizeData(self,i):
+        plt.plot(self.x[i],self.y[i],'g^')
+        return fig
+    '''    
     def motorBasis(self,numofbasis=None):
         #I will setup a hand-coded basis that is left power, right power, time
         if numofbasis is None:
@@ -123,10 +141,10 @@ class SesemoAtom:
     '''
     def whatDoISee2(self,var):
         #Extract variables        
-        G = var[0:80]
-        G = G.reshape([8,10])
-        M = var[80:]
-        M = M.reshape([10,2])
+        G = var[0:np.shape(self.G)[0]*np.shape(self.G)[1]]
+        G = G.reshape([np.shape(self.G)[0],np.shape(self.G)[1]])
+        M = var[np.shape(self.G)[0]*np.shape(self.G)[1]:]
+        M = M.reshape([np.shape(self.M)[0],np.shape(self.M)[1]])
         #compute beta
         beta = np.dot(self.alpha[self.TimeIdx],G)
         
@@ -139,7 +157,7 @@ class SesemoAtom:
     '''
     def whatDoISee3(self,var):
         M = var
-        M = M.reshape([10,2])
+        M = M.reshape([np.shape(self.M)[0],np.shape(self.M)[1]])
         beta = np.dot(self.alpha[self.TimeIdx],self.G)
         #Calculate error
         dist_from_self = np.linalg.norm(self.data -(self.center+np.dot(beta,M)))        
@@ -156,12 +174,19 @@ class SesemoAtom:
         obj2 = self.lam1*np.sum(np.absolute(alpha))                
         obj = obj1 + obj2        
         return obj
+        
+
+    def updateSensory(self,data):
+        grad = -2*np.dot(self.alpha[self.TimeIdx],(data-np.dot(self.S,self.alpha[self.TimeIdx])))
+        update = self.LR*grad
+        self.S = self.S + update
+        return np.linalg.norm(update)
     
     
     def learnmodel(self):
         self.TimeIdx = 0
-        EXPT = 2
-        for i in range(0,self.learnIterations):
+        EXPT = 4
+        for i in range(0,self.learnIterations-2):
             #Let's party!
             #PIck out Data
             data = np.zeros([1,2])
@@ -182,22 +207,28 @@ class SesemoAtom:
                 res = minimize(self.whatDoISee2,var,method='BFGS',jac=None,tol=1e-3,options={'disp':True,'maxiter':10})            
                 #print(res.x)
                 var = res.x
-                G = var[0:80]
-                self.G = G.reshape([8,10])
-                M = var[80:]
-                self.M = M.reshape([10,2])
+                G = var[0:np.shape(self.G)[0]*np.shape(self.G)[1]]
+                self.G = G.reshape([np.shape(self.G)[0],np.shape(self.G)[1]])
+                M = var[np.shape(self.G)[0]*np.shape(self.G)[1]:]
+                self.M = M.reshape([np.shape(self.M)[0],np.shape(self.M)[1]])
                 self.beta[self.TimeIdx+1] = np.dot(self.alpha[self.TimeIdx],self.G)
             elif EXPT is 2:
                 M = self.M.flatten()
-                print('Solving for M,G')
+                print('Solving for M')
                 res = minimize(self.whatDoISee3,M,method='BFGS',jac=None,tol=1e-3,options={'disp':True,'maxiter':10})            
-                self.M = res.x.reshape([10,2])
+                self.M = res.x.reshape([np.shape(self.M)[0],np.shape(self.M)[1]])
                 self.beta[self.TimeIdx+1] = np.dot(self.alpha[self.TimeIdx],self.G)
+            elif EXPT is 4:
+                print('Solving for M,S')
+                #update S
+                #Then infer the coefficients
+                #Solve for M
             '''            
             if self.DEBUG is True:
                 print('value of beta is %f')
                 print(res.x)
             '''
+            error = np.linalg.norm(data-self.center)
             self.center = self.center + np.dot(self.beta[self.TimeIdx+1],self.M)
             if self.DEBUG is True:        
                 print 'Value of data'
@@ -206,7 +237,8 @@ class SesemoAtom:
                 print self.center
                 #Let's calculateho far we are from center to Point
                 print '*******Error******'
-                print(np.linalg.norm(data-self.center))
+                print(error)
+            self.Error[self.TimeIdx] = error
             self.TimeIdx = self.TimeIdx + 1
         return 1
         
